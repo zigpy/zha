323080b58efbc7434853e4b23e6b2f8ee475e31d
David Mulcahey <david.mulcahey@me.com>
2024-08-30 18:31:57 -0400

    remove unused pylint disables

--- zha/application/platforms/sensor/__init__.py
+++ zha/application/platforms/sensor/__init__.py
@@ -247,8 +247,7 @@ class Sensor(PlatformEntity):
         return self.formatter(raw_state)
 
     def handle_cluster_handler_attribute_updated(
-        self,
-        event: ClusterAttributeUpdatedEvent,  # pylint: disable=unused-argument
+        self, event: ClusterAttributeUpdatedEvent
     ) -> None:
         """Handle attribute updates from the cluster handler."""
         if (
@@ -312,7 +311,7 @@ class PollableSensor(Sensor):
 
     def handle_cluster_handler_attribute_updated(
         self,
-        event: ClusterAttributeUpdatedEvent,  # pylint: disable=unused-argument
+        event: ClusterAttributeUpdatedEvent,
     ) -> None:
         """Handle attribute updates from the cluster handler."""
         super().handle_cluster_handler_attribute_updated(event)
