484e98c689c87304abf0ffba57a2c6e0d5b0b2e2
puddly <32534428+puddly@users.noreply.github.com>
2024-09-25 09:52:04 -0400

    Support `fallback_name`

--- zha/application/platforms/__init__.py
+++ zha/application/platforms/__init__.py
@@ -343,6 +343,7 @@ class PlatformEntity(BaseEntity):
         has_device_class = hasattr(entity_metadata, "device_class")
         has_attribute_name = hasattr(entity_metadata, "attribute_name")
         has_command_name = hasattr(entity_metadata, "command_name")
+        has_fallback_name = hasattr(entity_metadata, "fallback_name")
 
         if not has_device_class or entity_metadata.device_class is None:
             if entity_metadata.translation_key:
@@ -351,6 +352,8 @@ class PlatformEntity(BaseEntity):
                 self._attr_translation_key = entity_metadata.attribute_name
             elif has_command_name:
                 self._attr_translation_key = entity_metadata.command_name
+            elif has_fallback_name:
+                self._attr_fallback_name = entity_metadata.fallback_name
 
         if has_attribute_name:
             self._unique_id_suffix = entity_metadata.attribute_name
