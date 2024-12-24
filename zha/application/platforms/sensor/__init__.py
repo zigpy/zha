a0296f564c4c0490d60ed1993c0cb3c6108cdb8b
abmantis <amfcalt@gmail.com>
2024-12-24 16:03:03 +0000

    Use ph B as base for C to reduce duplicated attrs

--- zha/application/platforms/sensor/__init__.py
+++ zha/application/platforms/sensor/__init__.py
@@ -714,13 +714,12 @@ class ElectricalMeasurementRMSCurrentPhB(ElectricalMeasurementRMSCurrent):
 
 
 @MULTI_MATCH(cluster_handler_names=CLUSTER_HANDLER_ELECTRICAL_MEASUREMENT)
-class ElectricalMeasurementRMSCurrentPhC(ElectricalMeasurementRMSCurrent):
+class ElectricalMeasurementRMSCurrentPhC(ElectricalMeasurementRMSCurrentPhB):
     """RMS current measurement."""
 
     _attribute_name: str = "rms_current_ph_c"
     _unique_id_suffix: str = "rms_current_ph_c"
     _attr_translation_key: str = "rms_current_ph_c"
-    _skip_creation_if_none = True
 
     @property
     def _max_attribute_name(self) -> str:
