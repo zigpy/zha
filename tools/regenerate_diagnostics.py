47a0dec4eb7bac8e79f05a3ceb8c99efbcceead5
puddly <32534428+puddly@users.noreply.github.com>
2025-04-22 17:12:20 -0400

    Make the diagnostic regeneration tool prettier

--- tools/regenerate_diagnostics.py
+++ tools/regenerate_diagnostics.py
@@ -7,6 +7,7 @@ sys.path.insert(0, "tests")
 import asyncio
 import contextlib
 import json
+import logging
 import pathlib
 from unittest.mock import patch
 
@@ -14,6 +15,7 @@ from tests.common import ZhaJsonEncoder, join_zigpy_device, zigpy_device_from_js
 from tests.conftest import TestGateway, make_zha_data, make_zigpy_app_controller
 
 REPO_ROOT = pathlib.Path(__file__).parent.parent
+_LOGGER = logging.getLogger(__name__)
 
 
 @contextlib.asynccontextmanager
@@ -28,10 +30,16 @@ async def create_zha_gateway():
             yield zha_gateway
 
 
-async def main():
+async def main(files: list[str] | None = None) -> None:
     """Entry point."""
+    if files is None:
+        paths = list((REPO_ROOT / "tests" / "data" / "devices").glob("**/*.json"))
+    else:
+        paths = [pathlib.Path(f) for f in files]
+
     async with create_zha_gateway() as zha_gateway:
-        for device_json in (REPO_ROOT / "tests" / "data" / "devices").glob("**/*.json"):
+        for device_json in paths:
+            _LOGGER.info("Migrating %s", device_json)
             zigpy_device = await zigpy_device_from_json(
                 zha_gateway.application_controller,
                 device_json,
@@ -39,13 +47,22 @@ async def main():
 
             with patch("zigpy.zcl.Cluster._update_attribute"):
                 zha_device = await join_zigpy_device(zha_gateway, zigpy_device)
-                await zha_gateway.async_block_till_done(wait_background_tasks=True)
 
             new_json = json.dumps(
                 zha_device.get_diagnostics_json(), indent=2, cls=ZhaJsonEncoder
             )
             device_json.write_text(new_json)
 
+            await zha_device.on_remove()
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import coloredlogs
+
+    coloredlogs.install(level="DEBUG")
+
+    asyncio.run(
+        main(
+            files=(sys.argv[1:] if len(sys.argv) > 1 else None),
+        )
+    )
