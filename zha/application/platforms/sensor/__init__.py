c8518a69cd37314374185d26176d56d9400024fc
puddly <32534428+puddly@users.noreply.github.com>
2025-04-29 17:34:15 -0400

    Use the correct attribute definition

--- zha/application/platforms/sensor/__init__.py
+++ zha/application/platforms/sensor/__init__.py
@@ -21,6 +21,7 @@ from zigpy.state import Counter, State
 from zigpy.zcl import foundation
 from zigpy.zcl.clusters.closures import WindowCovering
 from zigpy.zcl.clusters.general import Basic
+from zigpy.zcl.clusters.smartenergy import Metering
 
 from zha.application import Platform
 from zha.application.platforms import (
@@ -309,12 +310,17 @@ class Sensor(PlatformEntity):
         ):
             self.maybe_emit_state_changed_event()
 
-    def _is_non_value(self, value: int | float) -> bool:
+    def _is_non_value(
+        self, value: int | float, *, attr_def: foundation.ZCLAttributeDef | None = None
+    ) -> bool:
         """Ignore non-value numerical values."""
-        if self._attr_def is None:
+        if attr_def is None:
+            attr_def = self._attr_def
+
+        if attr_def is None:
             return False
 
-        data_type = foundation.DataType.from_type_id(self._attr_def.zcl_type)
+        data_type = foundation.DataType.from_type_id(attr_def.zcl_type)
         return value == data_type.non_value
 
     def formatter(
@@ -1001,7 +1007,7 @@ class SmartEnergyMetering(PollableSensor):
 
     def _is_supported(self) -> bool:
         unit = self._cluster_handler.unit_of_measurement
-        if self._is_non_value(unit):
+        if self._is_non_value(unit, attr_def=Metering.AttributeDefs.unit_of_measure):
             return False
 
         return super()._is_supported()
