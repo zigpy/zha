5123d4a13613ce2857edf8acf6cf0c37b0f4f6ee
puddly <32534428+puddly@users.noreply.github.com>
2025-04-30 17:01:20 -0400

    Fix tests for entity naming

--- tests/test_registries.py
+++ tests/test_registries.py
@@ -560,20 +560,18 @@ def test_entity_names() -> None:
 
     for _, entity_classes in iter_all_rules():
         for entity_class in entity_classes:
-            if hasattr(entity_class, "_attr_fallback_name"):
-                # The entity has a name
+            if entity_class._attr_fallback_name is not None:
                 assert (
                     isinstance(entity_class._attr_fallback_name, str)
                     and entity_class._attr_fallback_name
                 )
-            elif hasattr(entity_class, "_attr_translation_key"):
+            elif entity_class._attr_translation_key is not None:
                 assert (
                     isinstance(entity_class._attr_translation_key, str)
                     and entity_class._attr_translation_key
                 )
-            elif hasattr(entity_class, "_attr_device_class"):
-                if entity_class is not AnalogInputSensor:
-                    assert entity_class._attr_device_class
+            elif entity_class._attr_device_class is not None:
+                pass
             else:
                 # The only exceptions
                 assert (
