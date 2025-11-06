ed4e985407f7a5922c207a47befaf278cbf818f2
Erik Montnemery <erik@montnemery.com>
2025-11-06 09:54:42 +0100

    Don't use magic number for gateway shut down delay

--- zha/application/gateway.py
+++ zha/application/gateway.py
@@ -58,6 +58,7 @@ from zha.zigbee.device import Device, DeviceInfo, DeviceStatus, ExtendedDeviceIn
 from zha.zigbee.group import Group, GroupInfo, GroupMemberReference
 
 BLOCK_LOG_TIMEOUT: Final[int] = 60
+SHUT_DOWN_DELAY_S: Final[int] = 0.1
 _R = TypeVar("_R")
 _LOGGER = logging.getLogger(__name__)
 
@@ -764,7 +765,7 @@ class Gateway(AsyncUtilMixin, EventBase):
         if self.application_controller is not None:
             await self.application_controller.shutdown()
             self.application_controller = None
-            await asyncio.sleep(0.1)  # give bellows thread callback a chance to run
+            await asyncio.sleep(SHUT_DOWN_DELAY_S)  # give bellows thread callback a chance to run
 
         await super().shutdown()
 
