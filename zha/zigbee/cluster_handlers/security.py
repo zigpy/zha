056ad85218442b2e95a8e89e151e153f058c4f58
puddly <32534428+puddly@users.noreply.github.com>
2026-01-21 00:37:55 -0500

    FIXME: Use a separate cluster handler for IAS ACE client clusters

--- zha/zigbee/cluster_handlers/security.py
+++ zha/zigbee/cluster_handlers/security.py
@@ -18,7 +18,12 @@ from zigpy.zcl.clusters.security import (
 )
 
 from zha.exceptions import ZHAException
-from zha.zigbee.cluster_handlers import ClusterHandler, ClusterHandlerStatus, registries
+from zha.zigbee.cluster_handlers import (
+    ClientClusterHandler,
+    ClusterHandler,
+    ClusterHandlerStatus,
+    registries,
+)
 from zha.zigbee.cluster_handlers.const import CLUSTER_HANDLER_STATE_CHANGED
 
 if TYPE_CHECKING:
@@ -36,7 +41,8 @@ class ClusterHandlerStateChangedEvent:
     event: Final[str] = "cluster_handler_state_changed"
 
 
-@registries.CLIENT_CLUSTER_HANDLER_REGISTRY.register(AceCluster.cluster_id)
+# TODO: is IasAce supposed to be a server cluster or a client cluster?
+@registries.CLUSTER_HANDLER_REGISTRY.register(AceCluster.cluster_id)
 class IasAceClusterHandler(ClusterHandler):
     """IAS Ancillary Control Equipment cluster handler."""
 
@@ -239,6 +245,12 @@ class IasAceClusterHandler(ClusterHandler):
         self, starting_zone_id, max_zone_ids, zone_status_mask_flag, zone_status_mask
     ):
         """Handle the IAS ACE zone status command."""
+
+
+# TODO: is IasAce supposed to be a server cluster or a client cluster?
+@registries.CLIENT_CLUSTER_HANDLER_REGISTRY.register(AceCluster.cluster_id)
+class IasAceClientClusterHandler(IasAceClusterHandler, ClientClusterHandler):
+    """IAS Ancillary Control Equipment client cluster handler."""
 
 
 @registries.CLUSTER_HANDLER_ONLY_CLUSTERS.register(IasWd.cluster_id)
